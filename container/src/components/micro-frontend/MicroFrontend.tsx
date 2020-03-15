d75e69f5e97642065063609c2da3f10f1de7ad0a
unknown <rmtestard@gmail.com>
2020-03-15 13:30:22 +0100

    I have moved back to only one script to load, we lose chunks lazy loading

--- container/src/components/micro-frontend/MicroFrontend.tsx
+++ container/src/components/micro-frontend/MicroFrontend.tsx
@@ -26,78 +26,38 @@ declare global {
 
 const MicroFrontend: React.FC<IProps> = ({ name, host, history }) => {
   const { data, loading, error } = useFetch(`${host}/asset-manifest.json`);
+
   useEffect(() => {
-    if (data && host) {
-      const { entrypoints } = data;
-      _loadExternalResources(host, entrypoints);
+    const scriptId = `micro-frontend-script-${name}`;
+    if (document.getElementById(scriptId)) {
+      renderMicroFrontend();
+      return;
     }
-  }, [data, host]);
 
-  /**
-   * Factory function to create script tags.
-   * @function _loadExternalResource
-   * @param {string} url - host url from which the bundle will be downloaded
-   * @param {string} entry - bundle location in the host
-   * @returns a script tag
-   */
+    if (data && host) {
+      const script = document.createElement('script');
+      script.id = scriptId;
+      script.crossOrigin = '';
+      script.src = _normalizeUrl(`${host}/${data.files['main.js']}`);
+      script.onload = renderMicroFrontend;
+      document.head.appendChild(script);
+    }
+  }, [data, host, name]);
 
-  const _loadExternalResource = (url: string, entry: string) => {
-    return new Promise((resolve, reject) => {
-      const match = entry.match(/\.([^.]+)$/);
-      if (!match) return;
-      const type = match[1];
-
-      let tag;
-      if (type === 'css') {
-        tag = document.createElement('link');
-        tag.type = 'text/css';
-        tag.rel = 'stylesheet';
-        tag.href = _normalizeUrl(`${url}/${entry}`);
-      } else if (type === 'js') {
-        tag = document.createElement('script');
-        tag.type = 'text/javascript';
-        tag.src = _normalizeUrl(`${url}/${entry}`);
-      }
-
-      if (tag) {
-        tag.id = uuidv4();
-        tag.onload = function() {
-          resolve();
-        };
-        tag.onerror = function() {
-          reject();
-        };
-
-
-        if (tag.type === 'test/javascript') {
-          document.getElementsByTagName('body')[0].appendChild(tag);
-        } else if (tag.type === 'test/javascript') {
-          document.getElementsByTagName('head')[0].appendChild(tag);
-        }
-      }
-    });
-  };
-
-  /**
-   * Factory function initializing a script for each chunk produced by webpack.
-   * @function _loadExternalResources
-   * @param {string} url - host url from which the bundle will be downloaded
-   * @param {array} entries - array of entries obtained from asset-manifest.json
-   * @returns an array of promises
-   */
-
-  const _loadExternalResources = (url: string, entries: string[]) => {
-    const promises = entries.map(entry => _loadExternalResource(url, entry));
-    return Promise.all(promises);
-  };
+  useEffect(() => {
+    return () => {
+      console.log('bye');
+      /*  window[`unmount${name}`](`${name}-container`); */
+    };
+  }, []);
 
   const _normalizeUrl = (path: string) => {
     return url.format(url.parse(path));
   };
 
   const renderMicroFrontend = () => {
-    console.log('hello')
-    window[`render${name}`](`${name}-container`, history);
+    console.log('hello');
+    /* window[`render${name}`](`${name}-container`, history); */
   };
 
   return (
